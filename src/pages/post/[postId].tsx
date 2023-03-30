68d50daf0083ee1bc69b46dca867c0310aa183c2
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-03-30 11:21:54 -0400

    add single post page and route

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -1,17 +1,58 @@
-import { type NextPage } from "next";
+import {
+  type GetServerSidePropsContext,
+  type InferGetServerSidePropsType,
+} from "next";
 import Head from "next/head";
 
-const SinglePostPage: NextPage = () => {
+import PageLayout from "~/components/layout";
+import { PostView } from "~/components/postView";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+import { api } from "~/utils/api";
+
+//* =============================================
+//*               PROFILE PAGE                  =
+//*==============================================
+export default function SinglePostPage({
+  postId,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const { data } = api.posts.getById.useQuery({
+    id: postId,
+  });
+
+  if (!data) return <div>Post not found</div>;
+
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>
+          {data.post.content} - @{data.author.username}
+        </title>
       </Head>
-      <main className="flex h-screen w-full justify-center">
-        <div>Single Post Page</div>
-      </main>
+      <PageLayout>
+        <PostView post={data.post} author={data.author} />
+      </PageLayout>
     </>
   );
-};
+}
 
-export default SinglePostPage;
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{ postId: string }>
+) {
+  const ssg = generateSSGHelper();
+
+  const postId = context.params?.postId;
+  if (typeof postId !== "string") throw new Error("Invalid postId");
+
+  /*
+   * `prefetch` does not return the result and never throws - if you need that behavior, use `fetch` instead.
+   */
+  await ssg.posts.getById.prefetch({ id: postId });
+
+  // Make sure to return { props: { trpcState: ssg.dehydrate() } }
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      postId,
+    },
+  };
+}
